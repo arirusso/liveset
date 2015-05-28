80501ff4b0cc54a16878abb2afd2f549d0ce4e52
Ari Russo <ari.russo@gmail.com>
2015-05-28 19:59:02 -0400

    first pass at player script

---
+++ bin/vidplayer.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+$:.unshift(File.join("lib"))
+
+require "liveset"
+
+@settings = {
+  :video => {
+    :directory => "media/video/vids/nosound/hifi",
+    :display => 0,
+    :is_fullscreen => false
+  },
+  :midi => {
+    :channel => 15,
+    :input => UniMIDI::Input.gets
+  }
+}
+
+@player = Liveset::VideoPlayer.new(@settings) do
+
+  rx_channel @settings[:midi][:channel]
+
+  #note("C3") { play() }
+  note("C#3") { play(video("pressure.mov")) }
+  note("D3") { play(video("spider.mov")) }
+  note("D#3") { play(video("skeleton.mov")) }
+  note("E3") { play(video("loop.mov")) }
+  note("F3") { play(video("bigbaby.mov")) }
+
+  cc(1) do |value|
+   percent = to_percent(value)
+   setting = [(percent / 100.0) * 2, 0.1].max
+   speed(setting, :set)
+ end
+
+end
+
+@player.start #(:background => true)
