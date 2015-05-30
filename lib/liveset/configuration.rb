29f2b36b06319a5804527bdeae49e51f2b1c78a5
Ari Russo <ari.russo@gmail.com>
2015-05-30 18:40:54 -0400

    vz server configuration file

--- lib/liveset/configuration.rb
+++ lib/liveset/configuration.rb
@@ -2,18 +2,35 @@ module Liveset
 
   class Configuration
 
-    attr_reader :vidplayer
+    DIR = {
+      :vidplayer => "config/vidplayer",
+      :vz => "config/vz"
+    }
+    FILE = {
+      :vidplayer => {
+        :settings => "#{DIR[:vidplayer]}/settings.yml",
+        :clips => "#{DIR[:vidplayer]}/clips.yml"
+      },
+      :vz => {
+        :server => "#{DIR[:vz]}/server.yml"
+      }
+    }
 
-    def initialize(settings_file, clips_file)
-      populate(settings_file, clips_file)
+    attr_reader :vidplayer, :vz
+
+    def initialize
+      populate
     end
 
     private
 
-    def populate(settings_file, clips_file)
+    def populate
       @vidplayer = {
-        :clips => YAML.load_file(clips_file).freeze,
-        :settings => YAML.load_file(settings_file).freeze
+        :clips => YAML.load_file(FILE[:vidplayer][:clips]).freeze,
+        :settings => YAML.load_file(FILE[:vidplayer][:settings]).freeze
+      }
+      @vz = {
+        :server => YAML.load_file(FILE[:vz][:server]).freeze
       }
     end
 
