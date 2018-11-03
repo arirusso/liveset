a15b372401b947f76b0f28fd1d988e62515af9fd
Ari Russo <ari.russo@gmail.com>
2018-11-03 16:49:59 -0400

    add launch script

---
+++ script/vidplayer/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#
+# Add to Startup Application Preferences:
+# gnome-terminal --profile=dev -e /home/vidplayer/liveset/script/vidplayer/launch.sh
+#
+
+cd workspace/liveset
+git checkout wl-demo-chromebox
+bundle install --path vendor/bundle || true
+bundle || true
+bundle exec script/vidplayer/listen
